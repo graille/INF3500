ff80ffbae7ca4f7db170c6df7a329c6be6c47e65
Benjamin O'Connell-Armand <benjamin.oconnell@polymtl.ca>
2013-11-12 10:33:48 -0500

    Ajout de fibonacci

---
+++ lab5/fibonacci.vhd
@@ -0,0 +1,22 @@
+---------------------------------------------------
+--  Polyechnique de Montréal
+--  Processeur spécifique de calcule de la série de Fibonacci
+--  Auteur: Christian Artin, Benjamin O'Connell-Armand
+--  Inf3500
+---------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+entity fibonacci is
+generic (
+	W : positive := 16
+);
+port(
+	clk, reset, go : in STD_LOGIC;
+	entree : in unsigned(W - 1 downto 0); -- n
+	Fn : out unsigned(W - 1 downto 0);
+	sortieValide : out std_logic
+);
+end fibonacci;
+
